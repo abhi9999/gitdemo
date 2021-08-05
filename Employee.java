8e21ab964388a7da99391c116ce2ca8f3b0f3a54
abhi9999 <manjrekarabhishek82@gmail.com>
2021-08-05 11:46:31 +0530

    emploee edited

--- Employee.java
+++ Employee.java
@@ -1,5 +1,10 @@
 public class empolyee{
 	public static void main(String[] args){
 	system.out.println("hello git");
+	system.out.println("add");
 	}
+	public static int add(int a, int b){
+		return a+b;
+	}
+	
 }
